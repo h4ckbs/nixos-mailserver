4f433496c8fb3e68480e161930e25d760888cc63
Antoine Eiche <lewo@abesis.fr>
2021-07-12 23:28:02 +0200

    ci: reenable 20.09 and 21.05 jobs :/
    They haven't been moved to flake so we still need to keep the non
    flake Hydra configuration.

--- .hydra/declarative-jobsets.nix
+++ .hydra/declarative-jobsets.nix
@@ -17,7 +17,36 @@ let
       flake = "gitlab:simple-nixos-mailserver/nixos-mailserver/merge-requests/${info.iid}/head";
     }
   ) prs;
+  # This could be removed once branch 20.09 and 21.05 would have been
+  # removed.
   mkJobset = branch: {
+    description = "Build ${branch} branch of Simple NixOS MailServer";
+    checkinterval = "60";
+    enabled = "1";
+    schedulingshares = 100;
+    enableemail = false;
+    emailoverride = "";
+    nixexprinput = "snm";
+    nixexprpath = ".hydra/default.nix";
+    type = 0;
+    inputs = {
+      # This is only used to allow Niv to use pkgs.fetchzip which is
+      # required because of Hydra restricted evaluation mode.
+      nixpkgs = {
+        value = "https://github.com/NixOS/nixpkgs b6eefa48d8e10491e43c0c6155ac12b463f6fed3";
+        type = "git";
+        emailresponsible = false;
+      };
+      snm = {
+        value = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver ${branch}";
+        type = "git";
+        emailresponsible = false;
+      };
+    };
+    keepnr = 3;
+    hidden = false;
+  };
+  mkFlakeJobset = branch: {
     description = "Build ${branch} branch of Simple NixOS MailServer";
     checkinterval = "60";
     enabled = "1";
@@ -31,7 +60,7 @@ let
   }; 
 
   desc = prJobsets // {
-    "master" = mkJobset "master";
+    "master" = mkFlakeJobset "master";
     "nixos-20.09" = mkJobset "nixos-20.09";
     "nixos-21.05" = mkJobset "nixos-21.05";
   };
